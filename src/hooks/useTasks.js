8df930f91b1428f914eebf5f7d512b4ccf024ea2
Glygalo Nikita <gnarrik@gmail.com>
2025-12-10 20:22:24 +0700

    Добавлена логика работы с сервером

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,67 +1,68 @@
 import {useCallback, useEffect, useMemo, useRef, useState} from 'react';
-import useTasksLocalStorage from './useTasksLocalStorage.js';
-
+import tasksAPI from '../api/tasksAPI';
 const useTasks = () => {
-  const {
-    savedTasks,
-    saveTasks,
-  } = useTasksLocalStorage();
 
-  const [tasks, setTasks] = useState(savedTasks ?? [
-    {id: 'task-1', title: 'Купить молоко', isDone: false,},
-    {id: 'task-2', title: 'Погладить кота', isDone: true,}
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
 
   const newTaskInputRef = useRef(null);
 
-
   const deleteAllTasks = useCallback(() => {
     const isConfirmed = confirm('Удалить все задачи?');
 
     if (isConfirmed) {
-      setTasks([]);
+      tasksAPI.deleteAll(tasks)
+        .then(() => setTasks([]))
     }
-  }, []);
+  }, [tasks]);
 
   const deleteTask = useCallback((taskId) => {
-    setTasks(
-      tasks.filter((task) => task.id !== taskId)
-    );
+    tasksAPI.delete(taskId)
+      .then(() => {
+        setTasks(
+          tasks.filter((task) => task.id !== taskId)
+        );
+      })
   }, [tasks]);
 
   const toggleTaskComplete = useCallback((taskId, isDone) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === taskId) {
-          return {...task, isDone};
-        } else {
-          return task;
-        }
+    tasksAPI.toggleComplete(taskId, isDone)
+      .then(() => {
+        setTasks(
+          tasks.map((task) => {
+            if (task.id === taskId) {
+              return {...task, isDone};
+            } else {
+              return task;
+            }
+          })
+        );
       })
-    );
+
   }, [tasks]);
 
   const addTask = useCallback((title) => {
     const newTask = {
-      id: crypto?.randomUUID() ?? Date.now().toString(),
       title,
       isDone: false,
     };
 
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-    setNewTaskTitle('');
-    setSearchQuery('');
-    newTaskInputRef.current.focus();
+    tasksAPI.add(newTask)
+      .then((addedTask) => {
+        setTasks((prevTasks) => [...prevTasks, addedTask]);
+        setNewTaskTitle('');
+        setSearchQuery('');
+        newTaskInputRef.current.focus();
+      })
+
+
   }, []);
 
   useEffect(() => {
-    saveTasks(tasks);
-  }, [tasks]);
-
-  useEffect(() => {
     newTaskInputRef.current.focus();
+
+    tasksAPI.getAll().then(setTasks)
   }, []);
 
   const filteredTasks = useMemo(() => {
@@ -71,6 +72,7 @@ const useTasks = () => {
       ? tasks.filter(({title}) => title.toLowerCase().includes(clearSearchQuery))
       : null;
   }, [searchQuery, tasks]);
+
 
   return (
     {
