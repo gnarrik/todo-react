fe1ee61c62f39df2eb8cd5d020d3674e875a09e1
Glygalo Nikita <gnarrik@gmail.com>
2025-12-11 20:34:40 +0700

    Добавлена маршрутизация

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Todo from './components/Todo.jsx';
-import {TasksProvider} from './context/TasksContext.jsx';
+import Router from './Router';
+import TasksPage from './pages/TasksPage';
+import TaskPage from './pages/TaskPage';
 
 const App = () => {
+  const routes = {
+    '/': TasksPage,
+    '/tasks/:id': TaskPage,
+    '*': () => <div>404 page not found</div>
+  }
+
   return (
-    <TasksProvider>
-      <Todo />
-    </TasksProvider>
+    <Router routes={routes}/>
   );
 };
 
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,6 @@
 import {memo, useContext} from 'react';
 import {TasksContext} from '../context/TasksContext.jsx';
+import RouterLink from './RouterLink';
 
 const TodoItem = (props) => {
   const {
@@ -29,11 +30,17 @@ const TodoItem = (props) => {
         onChange={({target}) => toggleTaskComplete(id, target.checked)}
       />
       <label
-        className="todo-item__label"
+        className="todo-item__label visually-hidden"
         htmlFor={id}
       >
         {title}
       </label>
+      <RouterLink
+        to={`/tasks/${id}`}
+        aria-label='Task detail page'
+      >
+        {title}
+      </RouterLink>
       <button
         className="todo-item__delete-button"
         aria-label="Delete"
